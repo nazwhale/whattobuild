7467315d511271c02755db65d2a426c3fb3fbb59
Naz Malik <nazmalik08@gmail.com>
2025-05-28 11:57:34 +0100

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": resolve("./src"),
     },
   },
   test: {
